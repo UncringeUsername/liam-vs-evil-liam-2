8392262063ea3a7cb4384fccf98ae260e7a57ea7
UncringeUsername” <thomasolav.griffithjones@gmail.com>
2026-05-07 11:45:49 +1200

    initial commit

---
+++ main.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class main
+     */
+    public main()
+    {
+        System.out.println("hihiiiii");
+    }
+}
