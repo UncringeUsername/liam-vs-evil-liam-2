329512663c8591fb6654730324dfa916c4d28cbc
UncringeUsername” <thomasolav.griffithjones@gmail.com>
2026-06-15 10:40:34 +1200

    stuff added

--- main.java
+++ main.java
@@ -1,5 +1,5 @@
 /**
- * Liam vs Evil Liam 2: Ultimate edition is a great advancement into the artistic medium of gaming.
+ * Liam vs Evil Liam 2: Ultimate edition isn't just a simple game; It is a great advancement into the artistic medium of gaming.
  * Fueled with the blood, sweat and tears of many programmers, it delivers a unique experience catered towards casual players and extreme gamers alike.
  */
 
@@ -59,25 +59,27 @@ public class main
     };
 
     int[][] wallsTwo = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-        {0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
-        {0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
-        {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     };
 
     // current walls are visible, collidable ones
     // alt and current flip when space key pressed
     private int[][] currentWalls = wallsOne;
     private int[][] altWalls = wallsTwo;
+
+    private int currentLevel = 0;
 
     private boolean altImages = false;
 
@@ -115,25 +117,25 @@ public class main
 
         frame.addKeyListener(new TAdapter());
 
-        loadImages();
+        LoadImages();
 
-        resetPlayerPosition();
+        ResetPlayerPosition();
 
-        prepareLevel("1,3,00000000000000000000000000000000000000000000000000000000000000000000000010000000000001000000000000000000000000001110000111100100000000000010200020000000000000010000000000000000010000020000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000111110011010011111000000001111100000000111110000000011111");
+        PrepareLevel("1,3,00000000000000000000000000000000000000000000000000000000000000000000000010000000000001000000000000000000000000001110000111100100000000000010200020000000000000010000000000000000010000020000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000111110011010011111000000001111100000000111110000000011111-0,3,11111111110000000000000000000000000000000000000000000000000000000000000010000000000001000000000000000000000000001110000111100100000000000010200020000000000000010000000000000000010000020000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000111110011010011111000000001111100000000111110000000011111");
 
-        prepareLevel(0);
+        LoadLevel(0);
 
-        update(frame);
+        Update(frame);
 
-        // timer which calls 'update' fps times per second
-        Timer updateTimer = new Timer((int) (1000.0 / fps), e -> update(frame));
+        // timer which calls 'Update' fps times per second
+        Timer updateTimer = new Timer((int) (1000.0 / fps), e -> Update(frame));
 
         updateTimer.start();
     }
 
     private String images_folder = "images";
 
-    private void loadImages() {
+    private void LoadImages() {
         playerImage = new ImageIcon(images_folder + "/player.png");
         wallActiveImageOne = new ImageIcon(images_folder + "/wall_active_1.png");
         wallInactiveImageOne = new ImageIcon(images_folder + "/wall_inactive_1.png");
@@ -145,7 +147,7 @@ public class main
         winInactiveImage = new ImageIcon(images_folder + "/win_inactive_1.png");
     }
 
-    private void resetPlayerPosition() {
+    private void ResetPlayerPosition() {
         playerX = playerStartX;
         playerY = playerStartY;
         playerXSmooth = playerStartX;
@@ -153,7 +155,7 @@ public class main
     }
 
     // runs every frame, the input 'frame' variable is the window
-    private void update(JFrame frame) {
+    private void Update(JFrame frame) {
 
         BufferStrategy bufferStrategy = frame.getBufferStrategy();
         if (bufferStrategy == null) {
@@ -187,6 +189,7 @@ public class main
             for (int tileX = 0; tileX < currentWalls.length; tileX++) {
                 if (currentWalls[tileY][tileX] == 2 && Math.abs(playerXSmooth - tileX) < 0.5 && Math.abs(playerYSmooth - tileY) < 0.5) {
                     System.out.println("RAAAHHHH WIN WIN WIN");
+                    LoadNextLevel();
                 } else if (playerXSmooth >= 1 && playerYSmooth >= 1 && tileX > 0 && tileY > 0 ) {
                     if (currentWalls[tileY - 1][tileX - 1] == 2 && Math.abs(playerXSmooth - tileX) < 0.5 && Math.abs(playerYSmooth - tileY) < 0.5) {
                         System.out.println("RAAAHHHH WIN WIN WIN");
@@ -276,7 +279,7 @@ public class main
 
         // if player inside wall, reset player position
         if (currentWalls[playerY][playerX] == 1) {
-            resetPlayerPosition();
+            ResetPlayerPosition();
         }
     }
 
@@ -297,7 +300,7 @@ public class main
         System.out.println(playerX + "  " + playerY);
     }
 
-    private void prepareLevel(String input) {
+    private void PrepareLevel(String input) {
         // player starting X
         int startX = 0;
 
@@ -338,7 +341,7 @@ public class main
             Arrays.fill(row, 0);
         }
         
-        for (i = 0; i < input.length(); i++) {
+        for (i = 0; i < input.length() && i < (13 * 13 * 2); i++) {
 
             int currentNumber = input.charAt(i) - '0';
 
@@ -361,17 +364,44 @@ public class main
             if (i == 13 * 13 * 2 - 1) {
                 levelData.add(level);
                 i = input.length();
+
+                if (input.indexOf('-') >= 0) {
+                    PrepareLevel(input.substring(input.indexOf('-')));
+                }
             }
         }
         System.out.println("");
     }
 
-    private void prepareLevel(int levelNum){
-        wallsOne = levelData.get(levelNum * 2);
-        wallsTwo = levelData.get(levelNum * 2 + 1);
+    private void LoadNextLevel(){
+        if (currentLevel + 1 < levelData.size() / 2) {
+            LoadLevel(currentLevel + 1);
+        } else {
+            System.out.println("Game finished yayayayayya!");
+        }
+    }
 
-        playerStartX = playerData.get(levelNum)[0];
-        playerStartY = playerData.get(levelNum)[1];
+    private void LoadLevel(int levelNum){
+        System.out.println("level data size is " + levelData.size());
+        if (levelData.size() / 2 > levelNum) {
+            wallsOne = levelData.get(levelNum * 2);
+            wallsTwo = levelData.get(levelNum * 2 + 1);
+
+            currentWalls = wallsOne;
+            altWalls = wallsTwo;
+
+            altImages = false;
+
+            playerStartX = playerData.get(levelNum)[0];
+            playerStartY = playerData.get(levelNum)[1];
+
+            playerX = playerStartX;
+            playerY = playerStartY;
+
+            currentLevel = levelNum;
+        } else {
+            System.out.println("Attempting to load a level that doesn't exist!");
+        }
     }
 
     // Luckyzelle
